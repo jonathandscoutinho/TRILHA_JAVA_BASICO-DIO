cc9cba5b5c2c702ee493fdc21dc4193ef2dc83ac
Jonathan Coutinho <jonathandscoutinho@gmail.com>
2024-05-14 13:16:35 -0300

    atualização de arquivo

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,18 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //classe String que representa texto na aplicação
+
+        String meuNome = "JOANTHAN COUTINHO";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+    }
+}
